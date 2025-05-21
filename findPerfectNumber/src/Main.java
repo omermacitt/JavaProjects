29c7e5378fb29777c711fb143666ed486b4b1282
omermacit <omerm_turk42@hotmail.com>
2025-05-21 19:28:57 +0300

    Find perfect Number.

---
+++ findPerfectNumber/src/Main.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i < 1000; i++) {
+            if (isPerfectNumber(i)) {
+                System.out.println(">" + i + "<");
+            }
+        }
+    }
+    public static boolean isPerfectNumber(int number) {
+        int sum = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+        return sum == number;
+    }
+}
